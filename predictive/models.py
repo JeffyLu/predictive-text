3c914ebd15e70ab92d3609f49d421cfc5a736a76
JeffyLu <a214121@126.com>
2018-01-13 23:29:50 +0800

    sharding vocabulary relation

--- predictive/models.py
+++ predictive/models.py
@@ -20,9 +20,15 @@ class VocabularyRelation(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    SHARDING_PIECE = 4
+
     def __str__(self):
         return '{} {}: {}'.format(self.vocab.word, self.next_vocab.word,
                                   self.frequency)
+
+    class Meta:
+        unique_together = ['vocab_id', 'next_vocab_id']
+        abstract = True
 
     @property
     def vocab(self):
@@ -33,3 +39,16 @@ class VocabularyRelation(models.Model):
     def next_vocab(self):
         vocab = Vocabulary.objects.filter(pk=self.next_vocab_id).first()
         return vocab.word if vocab else ''
+
+    @classmethod
+    def get_sharding_model(cls, sharding_key):
+        piece = sharding_key % cls.SHARDING_PIECE
+        class Meta:
+            db_table = 'predictive_vocabularyrelation_{}'.format(piece)
+            unique_together = ['vocab_id', 'next_vocab_id']
+        attrs = {'__module__': cls.__module__, 'Meta': Meta}
+        return type('VocabularyRelation_{}'.format(piece), (cls, ), attrs)
+
+
+for i in range(VocabularyRelation.SHARDING_PIECE):
+    VocabularyRelation.get_sharding_model(i)
